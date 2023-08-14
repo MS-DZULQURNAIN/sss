92ef6237ec62dc8bcb189cafc81c67e1b266719b
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-14 11:27:20 +0700

    Update mongo.py

--- StringSessionBot/database/mongo.py
+++ StringSessionBot/database/mongo.py
@@ -1,4 +1,12 @@
 from pymongo import MongoClient
 from config import MONGO_URL, MONGO_DB
 
-async def cek(user_id: int) :
+MongoClient 
+
+async def cek(user_id : int):
+    ada = user_data.find_one({'_id': user_id})
+    return bool(found)
+
+async def add_user(user_id: int):
+    user_data.insert_one({'_id': user_id})
+    return
