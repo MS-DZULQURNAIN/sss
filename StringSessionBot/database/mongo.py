e4cac8e905a20d728fa865d352fb0362d7230ec8
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-14 11:33:31 +0700

    Update mongo.py

--- StringSessionBot/database/mongo.py
+++ StringSessionBot/database/mongo.py
@@ -7,11 +7,22 @@ database = dbclient[DB_NAME]
 
 user_data = database['users']
 
-
 async def cek(user_id : int):
     ada = user_data.find_one({'id': user_id})
     return bool(ada)
 
-async def add(user_id: int):
+async def tambah(user_id: int):
     user_data.insert_one({'id': user_id})
+    return
+
+async def semua():
+    user_docs = user_data.find()
+    user_ids = []
+    for doc in user_docs:
+        user_ids.append(doc['id'])
+        
+    return user_ids
+
+async def hapus(user_id: int):
+    user_data.delete_one({'id': user_id})
     return
